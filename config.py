ab3db70b446590e5278340c5e92ca78678b00ef8
RealzaJ0s3 <realzadavi@gmail.com>
2026-05-23 16:47:23 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+# ============================================
+# CONFIGURACION - CREDENCIALES SUPABASE
+# ============================================
+
+SUPABASE_URL = "https://nyihvqzyusipbtynmpzh.supabase.co"
+SUPABASE_KEY = "sb_publishable_wyN3Jd33ZjblZ3L_WD8FOg_4ZgucK-f"
+DATABASE_URL = "postgresql://postgres:Tesl%40369*1215@db.nyihvqzyusipbtynmpzh.supabase.co:5432/postgres"
